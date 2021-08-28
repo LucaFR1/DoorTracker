2c9058941a0eb0c81ae80be70dd1e6517c413d19
LucaFR <lucacon47@gmail.com>
2021-08-28 17:34:54 +0200

    RTC included

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
-
-/
 #include <Arduino.h>
+#include <Wire.h>
+#include "RTClib.h"
+RTC_DS1307 rtc;
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
 //#include "ArduinoLowPower.h"
 
@@ -10,9 +12,29 @@ float val; // digital input, low when door is closed
 
 
 void writeData(void){
+
+  DateTime now = rtc.now();
+
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(" (");
+  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  Serial.print(") ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.print(now.second(), DEC);
+  Serial.println();
+
   if(doorClosed){
     Serial.println("door was closed, now open");
-  }
+
+
+      }
   else{
     Serial.println("door was open, now closed");
   }
@@ -43,8 +65,22 @@ void handleDoorState(void){
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600);
+
   pinMode(SwitchDigital, INPUT_PULLUP);
+  while (!Serial); // for Leonardo/Micro/Zero
+  Serial.begin(9600);
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    while (1);
+  }
+  if (! rtc.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
 
 }
 
