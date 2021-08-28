e7dbaacde060aaaeab66b16050d3d70d9b28e176
LucaFR <lucacon47@gmail.com>
2021-08-28 18:40:08 +0200

    SD Card working

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,71 @@
+
 #include <Arduino.h>
 #include <Wire.h>
 #include "RTClib.h"
-RTC_DS1307 rtc;
-char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
+#include <SPI.h>
+#include <SD.h>
+
+RTC_DS1307 rtc;
+
+File myFile;
+const int SDchipSelect = 4;
+
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 //#include "ArduinoLowPower.h"
 
 #define SwitchDigital 7
 bool doorClosed;
 float val; // digital input, low when door is closed
 
-
-void writeData(void){
-
+void writeDate(void){
   DateTime now = rtc.now();
 
-  Serial.print(now.year(), DEC);
-  Serial.print('/');
-  Serial.print(now.month(), DEC);
-  Serial.print('/');
-  Serial.print(now.day(), DEC);
-  Serial.print(" (");
-  Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
-  Serial.print(") ");
-  Serial.print(now.hour(), DEC);
-  Serial.print(':');
-  Serial.print(now.minute(), DEC);
-  Serial.print(':');
-  Serial.print(now.second(), DEC);
-  Serial.println();
-
-  if(doorClosed){
-    Serial.println("door was closed, now open");
+  myFile.print(now.year(), DEC);
+  myFile.print('/');
+  myFile.print(now.month(), DEC);
+  myFile.print('/');
+  myFile.print(now.day(), DEC);
+  myFile.print(" (");
+  myFile.print(daysOfTheWeek[now.dayOfTheWeek()]);
+  myFile.print(") ");
+  myFile.print(now.hour(), DEC);
+  myFile.print(':');
+  myFile.print(now.minute(), DEC);
+  myFile.print(':');
+  myFile.print(now.second(), DEC);
+  myFile.print(",");
+}
 
 
-      }
-  else{
-    Serial.println("door was open, now closed");
+void writeStart(void){
+  myFile = SD.open("test.txt", FILE_WRITE);
+  if (myFile) {
+    writeDate();
+    myFile.println("Device was started");
+    myFile.close();
+    Serial.println("done.");
+  }
+
+
+}
+
+
+void writeData(void){
+  myFile = SD.open("test.txt", FILE_WRITE);
+  if (myFile) {
+    writeDate();
+    if(doorClosed){
+      myFile.println("door was closed, now open");
+      Serial.println("door was closed, now open");
+        }
+    else{
+      myFile.println("door was open, now closed");
+      Serial.println("door was open, now closed");
+    }
+    myFile.close();
+    Serial.println("done.");
   }
 }
 
@@ -75,12 +104,20 @@ void setup() {
   }
   if (! rtc.isrunning()) {
     Serial.println("RTC is NOT running!");
-    // following line sets the RTC to the date & time this sketch was compiled
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-    // This line sets the RTC with an explicit date & time, for example to set
-    // January 21, 2014 at 3am you would call:
-    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
   }
+
+  Serial.print("Initializing SD card...");
+  pinMode(SS, OUTPUT);
+  if (!SD.begin(SDchipSelect)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+
+  writeStart();
+
+
 
 }
 
